d3232894c19daada68bef2252eab821c7980d41b
codehz <git@hertz.moe>
2024-12-26 15:39:53 +0800

    add INCLUDE_DIR

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export * from "./src/World";
 export * from "./src/Entity";
 export * from "./src/ScriptedEntity";
 export * from "./src/Reflection";
+
+export const INCLUDE_DIR = new URL("c-src", import.meta.url).pathname;
