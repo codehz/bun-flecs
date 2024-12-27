210c381b92de65834eeb8cec3908a05c872f36cf
codehz <git@hertz.moe>
2024-12-27 11:29:24 +0800

    forward other arguments

--- src/Extension.ts
+++ src/Extension.ts
@@ -4,11 +4,13 @@ export const INCLUDE_DIR = new URL("../c-src", import.meta.url).pathname;
 
 export function loadExtension<const Fns extends Record<string, FFIFunction>>(
   filename: string,
-  symbols: Fns
+  symbols: Fns,
+  { include, library }: { include?: string[]; library?: string[] } = {}
 ): ConvertFns<Fns> {
   return cc({
     source: filename,
-    include: [INCLUDE_DIR],
+    include: include ? [INCLUDE_DIR, ...include] : [INCLUDE_DIR],
+    library,
     symbols,
   }).symbols;
 }
